e32a5aed87098190df5c657bb980c0988ff23f31
José Ricardo Piccinin (Riko) <102562670+RikoPiccinin@users.noreply.github.com>
2026-03-15 22:38:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("home");
 });
 
 
